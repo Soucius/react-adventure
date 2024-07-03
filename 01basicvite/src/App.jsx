e954c0e6f7d7e769b504630f09e3dd6b63e6b98e
Soucius <soucius.ggwp@gmail.com>
2024-07-03 18:06:53 +0300

    I made my own React for learning how it works in backend

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,12 +1,13 @@
 import Youtube from "./Youtube";
 
-
 function App() {
+  const username = "soucius";
+
   return (
     <>
-      <h1>Vite react app</h1>
+      <h1>Vite react app { 2 + 2 }</h1>
 
-      <h1>Vite react app</h1>
+      <h1>Vite react app {}</h1>
 
       <Youtube />
     </>
