5d8a87e05c2d04b0abd30185c60c589c2a4c0ee5
Soucius <soucius.ggwp@gmail.com>
2024-07-05 13:25:00 +0300

    Making portfolio app for understanding react router dom, links, navlinks and more...

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-purple-400'>ssss</h1>
+    </>
+  );
+}
+
+export default App;
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # start of react
 
-#### Making progress in basic things
+#### Making progress
